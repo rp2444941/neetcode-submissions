f46948f88bc5c4f633e440e9e9d6616f1393a179
Rudra Pratap <73624720+rp2444941@users.noreply.github.com>
2026-06-02 08:54:09 +0530

    Add: set-zeroes-in-matrix - submission-2

---
+++ Data Structures & Algorithms/set-zeroes-in-matrix/submission-2.java
@@ -0,0 +1,48 @@
+class Solution {
+    public void setZeroes(int[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int col0 = 1; // Pehle column ko track karne ke liye variable
+
+        // Step 1: Matrix ko traverse karke markers set karo
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == 0) {
+                    // Current row ko 0 mark karo
+                    matrix[i][0] = 0;
+
+                    // Current col ko 0 mark karo (agar 0th col hai toh col0 variable use karo)
+                    if (j != 0) {
+                        matrix[0][j] = 0;
+                    } else {
+                        col0 = 0;
+                    }
+                }
+            }
+        }
+
+        // Step 2: Inner matrix ko update karo (1st row aur 1st col ko chhodkar)
+        // Peeche se shuru karenge taaki markers pehle hi na badal jayein
+        for (int i = m - 1; i >= 1; i--) {
+            for (int j = n - 1; j >= 1; j--) {
+                if (matrix[i][0] == 0 || matrix[0][j] == 0) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+
+        // Step 3: Pehli row ko update karo agar zarurat ho
+        if (matrix[0][0] == 0) {
+            for (int j = 0; j < n; j++) {
+                matrix[0][j] = 0;
+            }
+        }
+
+        // Step 4: Pehle column ko update karo agar col0 == 0 hai
+        if (col0 == 0) {
+            for (int i = 0; i < m; i++) {
+                matrix[i][0] = 0;
+            }
+        }
+    }
+}
